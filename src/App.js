5dff57709ed86a539efa5c3e29a32464a0f2ec0d
Hackmoo <hackmo872@gmail.com>
2023-05-23 21:05:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './styles/game.scss'
 
 function App() {
+  const computeWinner = (cells) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        cells[a] &&
+        cells[a] === cells[b] &&
+        cells[a] === cells[c]
+      ) {
+        return [a, b, c]
+      }
+    }
+  }
+
+  let [currentSymbol, setCurrentSymbol] = useState('X')
+  let [cellsState,setCellsState] = useState([null,null,null,null,null,null,null,null,null])
+  if(computeWinner(cellsState)) {
+    alert(`${currentSymbol === 'X' ? 'O' : 'X'} has won`)
+    reset()
+ }
+ if(cellsState.every(el => el)){
+  alert(`Field is filled`)
+  reset()
+ }
+  function setCell(el) {
+    if(cellsState[el]){
+      return alert('Spot has been already taken')
+    }
+    let temp = [...cellsState]
+    temp[el] = currentSymbol
+    setCellsState(temp)
+    setCurrentSymbol(currentSymbol === 'X' ? 'O' : 'X')
+  }
+
+  function reset() {
+    setCellsState([null,null,null,null,null,null,null,null,null])
+    setCurrentSymbol('X')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div className="game">
+      <div className="insideGame">
+      <div className="label">Current turn {currentSymbol}</div>
+      <div className="cells">
+        {cellsState.map((el, i) => <button 
+        className="cell" 
+        onClick={() => setCell(i)}
+        ><span>{cellsState[i]}</span></button>)}
+      </div>
+      <button className="btn" onClick={() => reset()}>Reset</button>
+      </div>
     </div>
+  </div>
   );
 }
 
